e8d599ddd8a32232dafd435bbc60efad0a9d033c
anhdht <dh.tuananh.91@gmail.com>
2016-05-29 16:17:03 +0700

    - Update

--- scriptmodules/supplementary/emulationstation.sh
+++ scriptmodules/supplementary/emulationstation.sh
@@ -27,7 +27,7 @@ function depends_emulationstation() {
 function sources_emulationstation() {
     local repo="$1"
     local branch="$2"
-    [[ -z "$repo" ]] && repo="https://github.com/retropie/EmulationStation"
+    [[ -z "$repo" ]] && repo="https://github.com/dhtuananh91/EmulationStation.git"
     [[ -z "$branch" ]] && branch="master"
     gitPullOrClone "$md_build" "$repo" "$branch"
     # make sure libMali.so can be found so we use OpenGL ES
